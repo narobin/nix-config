defed764cf2210099dc972d5715040c39743f661
Noah Robinson <noah@Noahs-MacBook-Air-4.local>
2026-08-24 10:46:07 -0400

    added development environment

--- flake.nix
+++ flake.nix
@@ -1,10 +1,37 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.server01 = {
-      modules = [
-        ./services/ddclient.nix
-      ]
-    }
-  }
+  description = "nix config for Nazh and Cabine";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { nixpkgs }: {
+    nixosConfigurations."servitor.narobin.com" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./hosts/servitor.narobin.com.nix ];
+    };
+
+    nixosConfigurations."vrrr.narobin.com" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./hosts/vrrr.narobin.com.nix ];
+    };
+
+    # Define development environment for this project
+    devShells."aarch64-darwin".default =
+      let
+        pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+      in
+      pkgs.mkShell {
+        packages = with pkgs; [
+          nil
+          nixd
+          nixfmt-rfc-style
+          git
+        ];
+
+        shellHook = ''
+          export NIX_PATH="nixpkgs=${nixpkgs}"
+        '';
+      };
+  };
 }
