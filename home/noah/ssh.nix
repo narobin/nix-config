3bb47f2b543a57eb289f9c764380104141214434
Noah Robinson <noah@narobin.com>
2026-08-25 08:41:28 -0400

    Update ssh.nix

--- home/noah/ssh.nix
+++ home/noah/ssh.nix
@@ -2,9 +2,10 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
 
     settings = {
-      "Host *" = {
+      "*" = {
         IdentityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
       };
     };
