f930d803948e007742734d18fd72a0c52c0be544
Noah Robinson <noah@narobin.com>
2026-08-30 15:11:44 -0400

    set wireless config for vrrr

--- hosts/vrrr/default.nix
+++ hosts/vrrr/default.nix
@@ -6,8 +6,11 @@
     ../../home/noah
   ];
 
-  mySystem.enableGui = true;
-  
+  mySystem = {
+    enableGui = true;
+    enableWireless = true;
+  };
+
   networking.hostName = "vrrr.narobin.com";
   nixpkgs.hostPlatform = "x86_64-linux";
   system.stateVersion = "26.05";
