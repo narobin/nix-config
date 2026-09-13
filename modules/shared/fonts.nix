41eec091fc1c03ace154ffe02cbe4a916f307ddc
Noah Robinson <noah@narobin.com>
2026-09-13 14:26:02 -0400

    added fonts

--- modules/shared/fonts.nix
+++ modules/shared/fonts.nix
@@ -2,5 +2,13 @@
 {
   fonts.packages = with pkgs; [
     jetbrains-mono
+    inter
+    atkinson-hyperlegible
+    fira-code
+    et-book
+    source-serif
+    source-sans
+    (iosevka-bin.override { variant = "Aile"; })
+    (iosevka-bin.override { variant = "Etoile"; })
   ];
 }
