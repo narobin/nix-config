481495c843534d52dec5c1db6b02bfab015badc0
Noah Robinson <noah@narobin.com>
2026-09-20 18:49:18 -0400

    fixed cabine home config

--- home/cabine/default.nix
+++ home/cabine/default.nix
@@ -1,14 +1,31 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 let
   username = "cabine";
   isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
-  homeDir = if isDarwin then "Users/${username}" else "/home/${username}";
+  homeDir = if isDarwin then /Users/${username} else "/home/${username}";
 in
 {
   users.users.${username} = {
     home = homeDir;
     shell = pkgs.zsh;
+  }
+  // lib.optionalAttrs (!isDarwin) {
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+    ];
+    isNormalUser = true;
+    uid = 1001;
+    # openssh.authorizedKeys.keyFiles = [
+    #   ./resources/remote-access.pub
+    # ];
+    # hashedPasswordFile = config.sops.secrets."cabine/password".path;
   };
 
   home-manager.users.${username} = {
